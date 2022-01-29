a84083dbbab02c4ea7099472d895f0f200e2af26
Swati Goel <72129763+Swati002@users.noreply.github.com>
2022-01-30 01:18:35 +0530

    Update coronaDetails.js

--- coronaDetails.js
+++ coronaDetails.js
@@ -1,6 +1,5 @@
 const request = require('request')
 const cheerio = require('cheerio')
-    // console.error('before')
 
 request('https://www.worldometers.info/coronavirus/', cb);
 
@@ -11,24 +10,11 @@ function cb(error, response, html) {
         handleHtml(html);
     }
 }
-// cheerio --> cheerio gives us a selector tool, using a selector tool hum html code me kch bhi search kr skte hai
 function handleHtml(html) {
     let selectorTool = cheerio.load(html)
-        // console.log(selectorTool)
 
     let contentArr = selectorTool('.maincounter-number')
-        // console.log(contentArr)
-
-    // converting the above contentArr data into text (for this cheerio gives us .text function )  Without selector tool we can not use text function(or any cheerio function) as here all the cheerio functions are loaded to selector tool
-
-
-    // Using for loop to access data
-
-    // for (let i = 0; i < contentArr.length; i++) {
-    //     let data = selectorTool(contentArr[i]).text()
-    //     console.log('Total Cases: ', data)
-    // }
-
+      
     console.log('Recent Corona Cases details of India:')
     let totalCases = selectorTool(contentArr[0]).text()
     console.log('Total Cases: ', totalCases)
@@ -39,8 +25,3 @@ function handleHtml(html) {
     let totalRecoveries = selectorTool(contentArr[2]).text()
     console.log('Total Recoveries: ', totalRecoveries)
 }
-
-
-
-
-// console.error('after')
